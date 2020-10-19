fae68f7d738e0f4d3704857c234f0b12833b6fa4
Ayrton Borges Junior <ayrton.borges@outlook.com>
2020-10-19 09:15:51 -0300

    Padrão Repository

---
+++ APICatalogo/Repository/CategoriaRepository.cs
@@ -0,0 +1,19 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace APICatalogo.Repository
+{
+    public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
+    {
+        public CategoriaRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public IEnumerable<Categoria> GetCategoriasProdutos()
+        {
+            return Get().Include(p => p.Produtos);
+        }
+    }
+}
